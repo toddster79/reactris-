9ab54577eb252322a11128e4e72e581409603548
Todd Carlson <todd.carlson3030@me.com>
2020-05-20 15:02:41 -0600

    Added components for the login pages.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import Tetris from './components/Tetris'; 
+import { UserForm } from './components/login-form/UserForm'
+
+const App = () => (
+  <div className="App"> 
+    <UserForm />
+    
+  </div>
+);
+
+export default App;
